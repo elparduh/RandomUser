64e2472b830b52d663ea449cf2b08f259bfbfa3a
Juan Ticante Vicente <juan.ticante@globant.com>
2022-10-08 15:29:13 -0500

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Juan Ticante Vicente on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
